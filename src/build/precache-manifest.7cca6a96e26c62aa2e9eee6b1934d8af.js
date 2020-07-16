ff0e24d9486a440ef14f350c079ce8e527a11b69
Tiffany <tiffany@connectngo.com>
2020-07-16 12:36:09 -0400

    First commit of blog-app project

---
+++ src/build/precache-manifest.7cca6a96e26c62aa2e9eee6b1934d8af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56c273213b0773673407b4d8c0e1f90b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76369d81595d9a2cbdf3",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c5730bfbb97ddbddf4d4",
+    "url": "/static/js/2.997b2f44.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.997b2f44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76369d81595d9a2cbdf3",
+    "url": "/static/js/main.760f604a.chunk.js"
+  },
+  {
+    "revision": "caf43f0a14314da69f12",
+    "url": "/static/js/runtime-main.226f7c93.js"
+  }
+]);
